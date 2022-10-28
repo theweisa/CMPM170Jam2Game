70aaf6efbe3afafc1e5635f97b253c1810fbdac3
theweisa <weiandrew2012@gmail.com>
2022-10-28 12:32:40 -0700

    poggers

--- Assets/Scripts/GravityOrbit.cs
+++ Assets/Scripts/GravityOrbit.cs
@@ -10,7 +10,7 @@ public class GravityOrbit : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("poggers");
     }
 
     // Update is called once per frame

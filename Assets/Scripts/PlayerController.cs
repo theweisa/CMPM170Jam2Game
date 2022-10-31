b4cdd9cffb67ef92f13bf376baa0ed4e5d9e6b93
Andrew Wei <weiandrew2012@gmail.com>
2022-10-31 14:19:48 -0700

    working (?) movement and gravity

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -19,17 +19,27 @@ public class PlayerController : MonoBehaviour
         // get axis' of movement (?)
         float horiz = Input.GetAxis("Horizontal");
         float vert = Input.GetAxis("Vertical");
+        /*
+        Vector3 forward = cam.transform.forward;
+        Vector3 right = cam.transform.right;
 
-        if (Mathf.Abs(horiz) <= 0.1f && Mathf.Abs(vert) <= 0.1f) {
+        forward.y = 0f;
+        right.y = 0f;
+        forward.Normalize();
+        right.Normalize();
+
+        Vector3 relativeDir = vert * forward + horiz * right;
+        */
+        /*if (Mathf.Abs(horiz) <= 0.1f && Mathf.Abs(vert) <= 0.1f) {
             rb.velocity = Vector3.zero;
-        }
+        }*/
 
         Vector3 dir = new Vector3(horiz, 0f, vert).normalized;
-        // dir = cam.transform.TransformDirection(dir);
+        dir = transform.TransformDirection(dir);
 
-        dir = Quaternion.Euler(0,cam.transform.eulerAngles.y,0)*(dir*speed);
-
-        rb.velocity = dir;
-        // rb.AddForce(dir*speed);
+        //dir = Quaternion.Euler(0,cam.transform.eulerAngles.y,0)*(dir);
+        // rb.velocity = relativeDir*speed;
+        rb.velocity = dir*speed;
+        // transform.Translate(relativeDir*speed*Time.deltaTime);
     }
 }

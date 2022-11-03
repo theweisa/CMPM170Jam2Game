57d5612bf813efca175d1af02739010fa66fc90f
Chrispau <chrispau.hk@gmail.com>
2022-11-03 01:15:55 -0700

    Menu looks great! added night skybox to level

--- Assets/Scripts/AIEnemy.cs
+++ Assets/Scripts/AIEnemy.cs
@@ -5,15 +5,29 @@ using UnityEngine.AI;
 
 public class AIEnemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
+    public Transform player;
+    
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        //Check for sight?
+        //playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        var distance = Vector3.Distance(transform.position, player.position);
+        Debug.Log(distance);
+        if(distance <= 6f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.position, Time.deltaTime * 2);
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.position, Time.deltaTime * distance * 2);
+        }
+
+
+        //sees player
+        //if (playerInSightRange) ChasePlayer();
+        //attack/jumpscare?
+        //if (playerIn)
     }
 }
